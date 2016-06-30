4eb769d8f389fff79e1f09a7753986b6ace64423
jetsonhacks <jim@jetsonhacks.com>
2016-06-30 15:50:44 -0700

    Get rid of junk characters in script

--- installRazor.sh
+++ installRazor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Install the Razor IMU ROS package
-sudo apt−get install ros−indigo−razor−imu−9dof -y
+sudo apt-get install ros-indigo-razor-imu-9dof -y 
 # For visualizing, you'll need these python packages
 sudo apt-get install python-visual python-wxgtk2.8 -y
 # To put firmaware on the Razor, you'll need the Arduino software
